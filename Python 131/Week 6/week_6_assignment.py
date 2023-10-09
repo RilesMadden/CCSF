c774780a8f6b538bda79779349d7f44a65b07f42
Riley Madden <rileymadden7@gmail.com>
2023-10-09 14:34:06 -0700

    week 6 assignment and pr

--- Python 131/Week 6/week_6_assignment.py
+++ Python 131/Week 6/week_6_assignment.py
@@ -1,6 +1,13 @@
-import sys
+"Write a program that prints out the exact command line arguments it receives, unchanged, but reordered from last to first"
 
-variable_1 = sys.argv[1]
-print(variable_1)
-print ("Number of arguments:", len(sys.argv), "arguments")
-print ("Argument List:", str(sys.argv))
+import sys
+list_of_args = sys.argv[1:] # save incoming arguments as a list
+list_of_args.reverse() # reverse the list's order
+
+# To dispay our reversed order, I'll create an empty string and iterate through our now-reversed list, adding each argument to the string.
+
+arg_string = ''
+for arg in list_of_args:
+        arg_string += arg + ' ' # added space after each arg for easier readability
+
+print(arg_string) # display completed string
