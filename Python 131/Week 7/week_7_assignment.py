3d492e4e6ac4a8ae72af6597cac8918c58f763cd
Riley Madden <rileymadden7@gmail.com>
2023-10-11 18:34:47 -0700

    changed comments in code

--- Python 131/Week 7/week_7_assignment.py
+++ Python 131/Week 7/week_7_assignment.py
@@ -2,15 +2,14 @@
 
 import sys
 
-list_of_args = sys.argv[1:] # Save incoming arguments as a list. This could have duplicates and multiple data types, however.
+list_of_args = sys.argv[1:] # Save incoming arguments as a list but it might have duplicates and multiple data types.
 
-# First we'll check if any arguments were passed. If not, we'll do a one-off message.
-if len(list_of_args) == 0:
+if len(list_of_args) == 0: # Check if no arguments were passed and print special exception if so.
         print("No arguments passed.")
 else:
-        set_of_args = set([str(arg) for arg in list_of_args]) # Turning the list into a set to get rid of any duplicates. At the same time, changing each argument to a string to ensure no issues during sorting.
+        set_of_args = set([str(arg) for arg in list_of_args]) # Turning the list into a set to get rid of any duplicates and changing each argument to a string to$
 
-        arg_string = '' # To make the output read nicely, I'm creating an empty string after which I'll iterate through our sorted set to build our output string.
+        arg_string = '' # Create a nice, readable output.
         for arg in sorted(set_of_args):
                 arg_string += arg + ' '
 
