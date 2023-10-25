05acc1679a36523c85ce3fa7ea6215e4e58b1720
Riley Madden <rileymadden7@gmail.com>
2023-10-24 17:53:56 -0700

    week 9 assignment

---
+++ Python 131/Week 9/week_9_assignment.py
@@ -0,0 +1,11 @@
+"""Adapt the previous assignment to roll both dies one hundred times, indicating how many times the sum of the results was a multiple of three"""
+
+import random
+
+multiple_of_three_counter = 0
+
+for i in range(0,100):
+    if (random.randint(1,9) + random.randint(1,1000)) % 3 == 0:
+        multiple_of_three_counter += 1
+
+print(f"After 100 rolls, the sum of the rolls was a multiple of three {multiple_of_three_counter} times.")
