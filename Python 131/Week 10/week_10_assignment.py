3ad5bc328e4ff0afa0f5ecbdbebd57ccbc5ee16a
Riley Madden <rileymadden7@gmail.com>
2023-11-01 17:59:56 -0700

    week 10 assignment

---
+++ Python 131/Week 10/week_10_assignment.py
@@ -0,0 +1,14 @@
+# Write an adder program that prints the sum of all the integer command line arguments passed
+# ignoring any non-integers that may be mixed in 
+
+args_passed = ['1', '2', '5', 'a', '3.14', '10', 'bologna']
+
+sum_of_args = 0
+
+for arg in args_passed:
+    try:
+        sum_of_args += int(arg)
+    except ValueError:
+        pass
+
+print(f"The sum of all integers passed is: {sum_of_args}")
