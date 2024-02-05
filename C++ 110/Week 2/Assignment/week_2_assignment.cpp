dec55b3eaeccc6662ed699858325f1cafedc45e0
Riley Madden <rileymadden7@gmail.com>
2024-02-05 10:41:55 -0800

    update to code

--- C++ 110/Week 2/Assignment/week_2_assignment.cpp
+++ C++ 110/Week 2/Assignment/week_2_assignment.cpp
@@ -14,37 +14,82 @@ int main(){
 
     const int MIN_CARD_VALUE = 1;
     const int MAX_CARD_VALUE = 10;
-    int random_card, player_card_sum = 0, dealer_card_sum;
+    int random_card, player_card_sum = 0, dealer_card_sum = 0;
     bool bust = false;
+    char play_again = 'y';
     char hit_me = 'y';
 
-    // Player's turn:
-    cout << "Welcome to the Blackjack Barn! Let's get your hand started." << endl;
-    random_card = (rand() % (MAX_CARD_VALUE - MIN_CARD_VALUE + 1)) + MIN_CARD_VALUE;
-    player_card_sum += random_card;
-    cout << "Your first card is: " << random_card << endl;
+    while (play_again == 'y') {
 
-    random_card = (rand() % (MAX_CARD_VALUE - MIN_CARD_VALUE + 1)) + MIN_CARD_VALUE;
-    player_card_sum += random_card;
-    cout << "Your second card is: " << random_card << endl;
-    cout << "Your current total is: " << player_card_sum << endl;
+        // Player's turn:
+        cout << "Welcome to the Blackjack Barn! Let's get your hand started." << endl;
+        random_card = (rand() % (MAX_CARD_VALUE - MIN_CARD_VALUE + 1)) + MIN_CARD_VALUE;
+        player_card_sum += random_card;
+        cout << "Your first card is: " << random_card << endl;
 
-    while (bust == false && hit_me == 'y'){
-        cout << "Would you like to hit (y/n)?: " << endl;
-        cin >> hit_me;
-        if (hit_me == 'y') {
-            random_card = (rand() % (MAX_CARD_VALUE - MIN_CARD_VALUE + 1)) + MIN_CARD_VALUE;
-            player_card_sum += random_card;
-            cout << "Ok, hit! Your next card is: " << random_card << endl;
-            cout << "Your new total is: " << player_card_sum << endl;
+        random_card = (rand() % (MAX_CARD_VALUE - MIN_CARD_VALUE + 1)) + MIN_CARD_VALUE;
+        player_card_sum += random_card;
+        cout << "Your second card is: " << random_card << endl;
+        cout << "Your current total is: " << player_card_sum << endl;
+
+        while (bust == false && hit_me == 'y'){
+            cout << "Would you like to hit (y/n)?: ";
+            cin >> hit_me;
+            if (hit_me == 'y') {
+                random_card = (rand() % (MAX_CARD_VALUE - MIN_CARD_VALUE + 1)) + MIN_CARD_VALUE;
+                player_card_sum += random_card;
+                cout << "Ok, hit! Your next card is: " << random_card << endl;
+                cout << "Your new total is: " << player_card_sum << endl;
+            };
+            if (player_card_sum == 21) {
+                cout << "Nice! A blackjack! Let's see if the dealer can match." << endl;
+                hit_me = 'n';
+            }
+            else if (player_card_sum >= 22) {
+                cout << "Oh no! It's a bust!" << endl;
+                bust = true;
+            };
         };
-        if (player_card_sum == 21) {
-            cout << "Nice! A blackjack! Let's see if the dealer can match." << endl;
-            hit_me = 'n';
+
+        // Dealer's turn
+        if (bust == false){
+            cout << "Ok, you have a total of " << player_card_sum << ", let's see if you can beat the dealer." << endl;
+            random_card = (rand() % (MAX_CARD_VALUE - MIN_CARD_VALUE + 1)) + MIN_CARD_VALUE;
+            dealer_card_sum += random_card;
+            cout << "The dealer's first card is: " << random_card << endl;
+
+            random_card = (rand() % (MAX_CARD_VALUE - MIN_CARD_VALUE + 1)) + MIN_CARD_VALUE;
+            dealer_card_sum += random_card;
+            cout << "The dealer's second card is: " << random_card << endl;
+            cout << "Their current total is: " << dealer_card_sum << endl;
+
+            while (dealer_card_sum < 17) {
+                cout << "The dealer's total is less than 17, so they will hit." << endl;
+                random_card = (rand() % (MAX_CARD_VALUE - MIN_CARD_VALUE + 1)) + MIN_CARD_VALUE;
+                dealer_card_sum += random_card;
+                cout << "The dealer's hit card is: " << random_card << endl;
+                cout << "Their current total is: " << dealer_card_sum << endl;
+            };
+
+            // Determine winner
+            // if (player_card_sum > dealer_card_sum) {
+            //     cout << "Congratulations! You've won!" << endl;
+            //     cout << "Your score of " << player_card_sum << " beat the dealer's score of " << dealer_card_sum << "." << endl;
+            // };
+            // else if (dealer_card_sum > player_card_sum) {
+            //     cout << "The dealer won this time!" << endl;
+            //     cout << "Their score of " << dealer_card_sum << " beat your score of " << player_card_sum << "." << endl;
+            //     cout << "Better luck next time!" << endl;
+            // };
         }
-        else if (player_card_sum >= 22) {
-            cout << "Oh no! It's a bust!" << endl;
-            bust = true;
+
+        cout << "Would you like to play again (y/n)?:" << endl;
+        cin >> play_again;
+        if (play_again == 'y') {
+        bust = false;
+        hit_me = 'y';
+        player_card_sum = 0;
+        dealer_card_sum = 0;
         };
     };
    return 0;
